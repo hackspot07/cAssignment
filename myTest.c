ae8688effaf50894dd2eed3f4c3a3b2e9b49eea5
parmatma <pyadav.it@gmail.com>
2015-01-19 10:17:29 +0530

    test naming reformed

--- myTest.c
+++ myTest.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-void test_fibonacci_series(){
+void test_fibonacci_series_it_produce_series_upto_5_term(){
 	int *arrayOfFibo,i,term=5;
 	arrayOfFibo =(int*)malloc(sizeof(int)*term);
 	assertEqual(fibo(term,arrayOfFibo),1);
@@ -15,7 +15,7 @@ void test_fibonacci_series(){
 	free(arrayOfFibo);
 };
 
-void test_concanate_array1_array2__as_result_array(){
+void test_concanate_array1_array2__as_result_array_1_2_2_3(){
 	int a []={1,2},b[]={2,3};
 	int *result_array;
 	assertEqual(concat(a,2,b,2,&result_array),4);
@@ -26,7 +26,7 @@ void test_concanate_array1_array2__as_result_array(){
 	free(result_array);
 };
 
-void test_filter_should_give_100_and_90_in_an_array(){
+void test_filter_should_give_100_and_90_from_array_100_90_80(){
 	int *result_array;
 	int scores[]={100,90,80};
  	assertEqual(filter(scores,3,85,&result_array),2);
@@ -35,7 +35,7 @@ void test_filter_should_give_100_and_90_in_an_array(){
  	free(result_array);
 };
 
-void test_reverse_the_array(){
+void test_reverse_the_array_scores_as_42_12_54_43_23(){
 	int *array,length = 5,*result_array;
 	int scores [] = {23,43,54,12,42};
 	assertEqual(reverse(scores,5,&result_array),5);
@@ -45,7 +45,7 @@ void test_reverse_the_array(){
 	free(result_array);
 };
 
-void test_slice_the_array1_which_gives_the_array_from_2_to_5(){
+void test_slice_the_array1_which_gives_the_array_from_the_indexof_2_to_5(){
 	int length_of_array,start_index,end_index,*result_array;
 	int scores [] = {23,43,54,12,42,90};
 	assertEqual(slice(scores,6,2,5,&result_array),3);
@@ -55,7 +55,7 @@ void test_slice_the_array1_which_gives_the_array_from_2_to_5(){
 	free(result_array);
 };
 
-void test_slice_the_array1_which_gives_the_array_from_0_to_3_if_slice_with_minus6_4(){
+void test_slice_the_array1_which_gives_the_array_from_0_to_3_if_slice_with_minus_6_4(){
 	int start_index,end_index,*result_array;
 	int scores [] = {1,2,3,4,5,6,7,8,9,10};
 	assertEqual(slice(scores,10,-2,4,&result_array),4);
@@ -76,7 +76,7 @@ void test_give_the_4_numberOfprimes_between_1_to_10(){
 	free(result_array);
 };
 
-void test_give_the_4_numberOfprimes_between_2_to_1000(){
+void test_give_the_168_numberOfprimes_between_2_to_1000(){
 	int start=1,end=10,*result_array;
 	assertEqual(prime(2,1000,&result_array),168);
 	assertEqual(result_array[0],2);
@@ -85,7 +85,7 @@ void test_give_the_4_numberOfprimes_between_2_to_1000(){
 	free(result_array);
 };
 
-void test_give_the_4_numberOfprimes_between_2_to_5000(){
+void test_give_the_669_numberOfprimes_between_2_to_5000(){
 	int start=1,end=10,*result_array;
 	assertEqual(prime(2,5000,&result_array),669);
 	assertEqual(result_array[0],2);
@@ -94,7 +94,7 @@ void test_give_the_4_numberOfprimes_between_2_to_5000(){
 	free(result_array);
 };
 
-void test_give_the_4_numberOfprimes_between_2_to_15000(){
+void test_give_the_1754_numberOfprimes_between_2_to_15000(){
 	int start=1,end=10,*result_array;
 	assertEqual(prime(2,15000,&result_array),1754);
 	assertEqual(result_array[0],2);
@@ -109,13 +109,13 @@ void test_gives_the_hello_and_hello_are_same(){
 
 };
 
-void test_gives_the_raja_and_rani_are_not_same(){
+void test_gives_the_reja_and_rani_are_not_same(){
 	char *str1="reja",*str2="rani";
 	assertEqual(stringCompare(str1,str2),4);
 	assertEqual(stringCompare(str2,str1),-4);
 };
 
-void test_gives_hellllllllllloooo_and_hello_difference_is(){
+void test_gives_helloooo_and_hello_difference_is_111(){
 	char *str1="helloooo",*str2="hello";
 	assertEqual(stringCompare(str1,str2),111);
 	assertEqual(stringCompare(str2,str1),-111);
@@ -151,7 +151,7 @@ int isGreaterThan4(int x,int index,int* array){
 	return (x>4)?x:0;
 };
 
-void test_filter_return_5_6_7(){
+void test_filter_return_5_6_7_from_the_array(){
 	int *result_array;
 	int array[6] = {2,3,4,5,6,7};
 	int resultLength;
@@ -168,7 +168,7 @@ int isNotEqual(char* x,int index,char** array){
 	return (yes==0)?0:1;
 };
 
-void test_filter_return_lenth_3(){
+void test_filter_return_lenth_3_after_filter_the_array(){
 	char **result_array;
 	char *array[] = {"hi","hi","hi","hello","hello","man"};
 	int resultLength;
@@ -184,7 +184,7 @@ int multiplyBy2(int x,int index,int* array){
 	return x*2;
 };
 
-void test_map_returns_double_of_each_element(){
+void test_map_returns_the_value_after_multiplyby2_of_each_element(){
 	int array[6] = {2,3,4,5,6,0};
 	int *result_array;
 	result_array = intMap(array,6,multiplyBy2);
@@ -213,7 +213,7 @@ float multiplyBy3(float x,int index,float* array){
 	return x*3;
 };
 
-void test_floatmap_returns_double_of_each_element(){
+void test_floatmap_returns_the_value_after_multiplyby2_of_each_element(){
 	float array[6] = {2.1,3.7,4.8,5.0,6.9,0};
 	float *result_array;
 	result_array = floatMap(array,6,multiplyBy3);
@@ -242,7 +242,7 @@ char* stringConcat(char* x,char* y,int index,char** array ){
 };
 
 
-void test_reduceString_it_retunr_the_concanate_string(){
+void test_reduceString_it_retunr_the_concanate_string_TThihihihehe(){
 	char *array[] = {"hi","hi","hi","he","he"};
 	char *initialValue = "TT";
 	char *result;
@@ -266,7 +266,7 @@ char charConcat(char x,char y,int index,char* araray){
 	return (x>y)?x:y;
 };
 
-void test_reduceChar_it_return_Thello(){
+void test_reduceChar_it_returns_string_Thello(){
 	char array[] = {'h','e','l','l','o','\0'};
 	char initialValue = 'T';
 	char result;
@@ -282,7 +282,7 @@ void test_indexOf_will_return_the_2_index_of_r(){
 	assertEqual(index,2);
 };
 
-void test_stringindexOf_will_return_the_1_index_of_rma(){
+void test_stringindexOf_will_return_the_2_index_of_rma(){
 	char* string = "parmatma";
 	char* substring = "rma";
 	int index;
@@ -290,7 +290,7 @@ void test_stringindexOf_will_return_the_1_index_of_rma(){
 	assertEqual(index,2);
 };
 
-void test_stringindexOf_will_return_the_minus_1_index_of_ram(){
+void test_stringindexOf_will_return_the_minus_3_index_of_ram(){
 	char* string = "hello";
 	char* substring = "lo";
 	int index;
